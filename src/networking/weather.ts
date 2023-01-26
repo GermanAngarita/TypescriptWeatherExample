e9539d6eaee0bde6192bdcb96192f601868a867a
GermanAngarita <germanadolfoangarita@gmail.com>
2023-01-25 21:07:59 -0500

    feat: avance inicial actividad 4

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -1,7 +1,24 @@
 import { WeatherResponse } from "../model/weatherResponse";
 
-const city = 'Example';
 
-const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`;
+// const city = 'Example';
+
+
 
 // TODO: Create an async function with an argument called city to return the that of the endpoint
+
+export const getWeather = async ( city: string ) => {
+
+    const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`;
+
+    const init: RequestInit = {
+        method: "GET"
+    }
+
+    try {
+        const response = await fetch(API_CURRENT, init )
+        return response.json();
+    } catch (error) {
+        return error
+    }
+}
